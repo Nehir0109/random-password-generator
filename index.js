d64c70213894705304ebb04d9ed76c7e21f1a8d8
Nehir Çakılcı <nehir.cakilci@gmail.com>
2023-09-05 22:13:44 +0300

    button is set to be disabled at first

--- index.js
+++ index.js
@@ -29,24 +29,42 @@ function generatePassword() {
 generateBtn.disabled = true;
 
 lengthInput.addEventListener("change", (e) => {
-    if (e.target.value > 20) {
-      e.target.value = 20;
-    }
-
+  if (e.target.value > 20) {
+    e.target.value = 20;
+  }
+  if (e.target.value <= 0) {
+    generateBtn.disabled = true;
+    generateBtn.style.backgroundColor = "#737373";
+  } else {
     passwordLength = e.target.value;
     generateBtn.disabled = false;
-
+    generateBtn.style.backgroundColor = "#10B981";
+  }
 });
 
+function copy(copyElement) {
+  const currentElement = document.getElementById(copyElement);
+  let copyText = document.getElementById(copyElement).innerText; // result 1 ve 2'nin innerText'ini almak için
 
-function copy1() {
-    let copyText1 = document.getElementById("result1").innerText;
-    navigator.clipboard.writeText(copyText1);
-    alert(`Password Copied On Clipboard! :  ` + copyText1 );
+  if (copyText) {
+    navigator.clipboard.writeText(copyText); //yazıyı kopyalamak için
+    currentElement.innerText = "Copied!";
+    setTimeout(() => {
+      currentElement.innerText = copyText;
+    }, 1000);
+  } else {
+    alert("You need provide a number to generate a password");
   }
+}
 
-  function copy2() {
-    let copyText2 = document.getElementById("result2").innerText;
-    navigator.clipboard.writeText(copyText2);
-    alert(`Password Copied On Clipboard! :  ` + copyText2 );
-  }
+//function copy1() {
+    //let copyText1 = document.getElementById("result1").innerText;
+    //navigator.clipboard.writeText(copyText1);
+    //alert(`Password Copied On Clipboard! :  ` + copyText1 );
+  //}
+
+  //function copy2() {
+    //let copyText2 = document.getElementById("result2").innerText;
+    //navigator.clipboard.writeText(copyText2);
+    //alert(`Password Copied On Clipboard! :  ` + copyText2 );
+  //}
